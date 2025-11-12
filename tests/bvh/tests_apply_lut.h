6336638c1ec18cba9822df28fcc50df37c502856
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-11-12 18:19:18 +0100

    feat(Tests): add BVH apply swaps lut tests

---
+++ tests/bvh/tests_apply_lut.h
@@ -0,0 +1,8 @@
+#ifndef TESTS_APPLY_LUT_H_
+#define TESTS_APPLY_LUT_H_
+
+#include <stdbool.h>
+
+bool all_bvh_lut_tests(void);
+
+#endif // TESTS_APPLY_LUT_H_
--- tests/main.c
+++ tests/main.c
@@ -1,3 +1,4 @@
+#include "bvh/tests_apply_lut.h"
 #include "gltf/tests_gltf.h"
 #include "yaw_pitch/tests_yawpitch.h"
 #include "tests_macros.h"
@@ -5,5 +6,6 @@
 int main(void) {
   TEST_RUN(all_yawpitch_tests);
   TEST_RUN(all_gltf_tests);
+  TEST_RUN(all_bvh_lut_tests);
   return 0;
 }
