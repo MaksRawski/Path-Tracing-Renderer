3fa7a31c024b9b2ceaa4891d187e1703e131ada0
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-11-12 16:01:47 +0100

    refactor: cleanup tests
    tests_inputs -> tests_yawpitch

--- tests/main.c
+++ tests/main.c
@@ -1,9 +1,9 @@
 #include "gltf/tests_gltf.h"
-#include "inputs/tests_inputs.h"
+#include "yaw_pitch/tests_yawpitch.h"
 #include "tests_macros.h"
 
 int main(void) {
-  TEST_RUN(all_inputs_tests);
+  TEST_RUN(all_yawpitch_tests);
   TEST_RUN(all_gltf_tests);
   return 0;
 }
