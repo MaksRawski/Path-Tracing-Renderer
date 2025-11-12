80af1e54774dcc7a3e31fca15cd1ff662b30234a
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-11-13 00:10:11 +0100

    fix(tests): better apply_swaps_lut tests

--- tests/bvh/tests_apply_lut.c
+++ tests/bvh/tests_apply_lut.c
@@ -3,46 +3,59 @@
 #include "scene/bvh.h"
 #include "tests_macros.h"
 
-bool test_apply_swaps__id(void) {
+bool test_apply_swaps_lut__id(void) {
   BVHTriCount numbers[] = {0, 1, 2, 3, 4, 5};
   BVHTriCount lut[] = {0, 1, 2, 3, 4, 5};
   BVHTriCount expected[] = {0, 1, 2, 3, 4, 5};
 
-  const unsigned int size = sizeof(lut) / sizeof(BVHTriCount);
-  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, size);
+  const unsigned int count = sizeof(lut) / sizeof(BVHTriCount);
+  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, count);
 
-  ASSERT_ARRAYN_EQ(numbers, expected, BVHTriCount, size);
+  ASSERT_ARRAYN_EQ(numbers, expected, count);
   return true;
 }
 
-bool test_apply_swaps__rev(void) {
+bool test_apply_swaps_lut__rev(void) {
   BVHTriCount numbers[] = {0, 1, 2, 3, 4, 5};
   BVHTriCount lut[] = {5, 4, 3, 2, 1, 0};
   BVHTriCount expected[] = {5, 4, 3, 2, 1, 0};
 
-  const unsigned int size = sizeof(lut) / sizeof(BVHTriCount);
-  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, size);
+  const unsigned int count = sizeof(lut) / sizeof(BVHTriCount);
+  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, count);
 
-  ASSERT_ARRAYN_EQ(numbers, expected, BVHTriCount, size);
+  ASSERT_ARRAYN_EQ(numbers, expected, count);
   return true;
 }
 
-bool test_apply_swaps__random(void) {
+bool test_apply_swaps_lut__random(void) {
   BVHTriCount numbers[] = {0, 1, 2, 3, 4, 5};
   BVHTriCount lut[] = {2, 4, 1, 3, 5, 0};
   BVHTriCount expected[] = {2, 4, 1, 3, 5, 0};
+  const unsigned int count = sizeof(lut) / sizeof(BVHTriCount);
 
-  const unsigned int size = sizeof(lut) / sizeof(BVHTriCount);
-  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, size);
+  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, count);
 
-  ASSERT_ARRAYN_EQ(numbers, expected, BVHTriCount, size);
+  ASSERT_ARRAYN_EQ(numbers, expected, count);
+  return true;
+}
+
+bool test_apply_swaps_lut__simplest_earlier_breaking_case(void) {
+  BVHTriCount numbers[] = {0, 1, 2, 3, 4, 5};
+  BVHTriCount lut[] = {2, 0, 1, 3, 4, 5};
+  BVHTriCount expected[] = {2, 0, 1, 3, 4, 5};
+  const unsigned int count = sizeof(lut) / sizeof(BVHTriCount);
+
+  BVH_apply_swaps_lut(lut, numbers, BVHTriCount, count);
+
+  ASSERT_ARRAYN_EQ(numbers, expected, count);
   return true;
 }
 
 bool all_bvh_lut_tests(void) {
-  TEST_RUN(test_apply_swaps__id);
-  TEST_RUN(test_apply_swaps__rev);
-  TEST_RUN(test_apply_swaps__random);
+  TEST_RUN(test_apply_swaps_lut__id);
+  TEST_RUN(test_apply_swaps_lut__rev);
+  TEST_RUN(test_apply_swaps_lut__random);
+  TEST_RUN(test_apply_swaps_lut__simplest_earlier_breaking_case);
 
   return true;
 }
