e3a6b211e69c3aca8cc89c034ac90bb91583ea5d
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-12-21 16:44:44 +0100

    fix: GL_CALL

--- include/opengl/gl_call.h
+++ include/opengl/gl_call.h
@@ -1,12 +1,12 @@
 #ifndef CALL_MACRO_H_
 #define CALL_MACRO_H_
 
-void GL_CALL_impl(void);
+void GL_CALL_impl(const char *file_name, int line_num);
 
 #ifndef NDEBUG
 #define GL_CALL(_CALL)                                                         \
   _CALL;                                                                       \
-  GL_CALL_impl();
+  GL_CALL_impl(__FILE__, __LINE__);
 #else
 #define GL_CALL(_CALL) _CALL
 #endif
--- src/opengl/gl_call.c
+++ src/opengl/gl_call.c
@@ -2,8 +2,8 @@
 #include "glad/gl.h"
 #include <stdio.h>
 
-void GL_CALL_impl(void) {
+void GL_CALL_impl(const char *file_name, int line_num) {
   GLenum gl_err = glGetError();
   if (gl_err != 0)
-    fprintf(stderr, "%s:%d: OpenGL error 0x%x\n", __FILE__, __LINE__, gl_err);
+    fprintf(stderr, "%s:%d: OpenGL error 0x%x\n", file_name, line_num, gl_err);
 }
