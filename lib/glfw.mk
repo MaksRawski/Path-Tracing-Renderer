0374a61eedb6fef983e509245f083a702ef91009
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-12-22 01:32:07 +0100

    fix: using GLFW as a submodule
    again...

--- lib/glfw.mk
+++ lib/glfw.mk
@@ -3,6 +3,6 @@ include vars.mk
 GLFW_CMAKE_OPTIONS = -DGLFW_BUILD_EXAMPLES=0 \
 						-DGLFW_BUILD_TESTS=0
 
-$(GLFW_TARGET): glfw-3.4
+$(GLFW_TARGET): glfw
 	cmake -Bbuild/glfw $(GLFW_CMAKE_OPTIONS) $<
 	cmake --build build/glfw
