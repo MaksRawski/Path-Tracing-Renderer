e5bdae5172e679947ecbe303afcad3071f000e29
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-12-22 01:10:07 +0100

    fix: GL_CALL is now scoped

--- include/opengl/gl_call.h
+++ include/opengl/gl_call.h
@@ -5,8 +5,10 @@ void GL_CALL_impl(const char *file_name, int line_num);
 
 #ifndef NDEBUG
 #define GL_CALL(_CALL)                                                         \
-  _CALL;                                                                       \
-  GL_CALL_impl(__FILE__, __LINE__);
+  do {                                                                         \
+    _CALL;                                                                     \
+    GL_CALL_impl(__FILE__, __LINE__);                                          \
+  } while (0);
 #else
 #define GL_CALL(_CALL) _CALL
 #endif
