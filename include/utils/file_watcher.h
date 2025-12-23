bcabeda1c2ff0b702f5c634a35949aed04f4cd54
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-12-23 20:45:19 +0100

    fix(FileWatcher): use nanosecond precision
    this is using some transitional name of a field in struct stat
    so it's not even portable across UNIX systems

--- include/utils/file_watcher.h
+++ include/utils/file_watcher.h
@@ -1,15 +1,17 @@
 #ifndef FILE_WATCHER_H_
 #define FILE_WATCHER_H_
 #include <stdbool.h>
-#include <time.h>
 
 typedef struct {
   char *path;
-  time_t _mtime;
-  time_t _ctime;
+#ifdef __linux__
+  unsigned long _mtimensec;
+#else
+  long _mtime;
+#endif
 } FileWatcher;
 
-FileWatcher FileWatcher_new(const char * path);
+FileWatcher FileWatcher_new(const char *path);
 bool FileWatcher_did_change(FileWatcher *self);
 void FileWatcher_delete(FileWatcher *self);
 
--- src/utils/file_watcher.c
+++ src/utils/file_watcher.c
@@ -12,14 +12,37 @@
 #define stat _stat
 #endif
 
+// NOTE: using nanosecond precision on linux and second precision elsewhere
+// (which could very well be problematic but I don't care about that for now)
+// TODO: instead use OS specific APIs that offer more reliable mechanisms for
+// this
+
+void set_stats(FileWatcher *self, struct stat stats);
+bool stats_changed(FileWatcher *self, struct stat stats);
+
+void set_stats(FileWatcher *self, struct stat stats) {
+#ifdef __linux__
+  self->_mtimensec = stats.st_mtimensec;
+#else
+  self->_mtime = stats.st_mtime;
+#endif
+}
+
+bool stats_changed(FileWatcher *self, struct stat stats) {
+#ifdef __linux__
+  return self->_mtimensec != stats.st_mtimensec;
+#else
+  return self->_mtime != stats.st_mtime;
+#endif
+}
+
 // NOTE: exits if the file at path cannot be watched
 FileWatcher FileWatcher_new(const char *path) {
   FileWatcher self = {0};
 
   struct stat stats = {0};
   stat(path, &stats);
-  self._mtime = stats.st_mtime;
-  self._ctime = stats.st_ctime;
+  set_stats(&self, stats);
 
   self.path = (char *)malloc(strlen(path) + 1);
   strcpy(self.path, path);
@@ -34,12 +57,12 @@ bool FileWatcher_did_change(FileWatcher *self) {
     fprintf(stderr, "Failed to stat %s: %s\n", self->path, strerror(errno));
     exit(EXIT_FAILURE);
   }
-  bool did_change =
-      (self->_mtime != stats.st_mtime || self->_ctime != stats.st_ctime);
-  self->_mtime = stats.st_mtime;
-  self->_ctime = stats.st_ctime;
 
-  return did_change;
+  if (stats_changed(self, stats)) {
+    set_stats(self, stats);
+    return true;
+  }
+  return false;
 }
 
 void FileWatcher_delete(FileWatcher *self) {
