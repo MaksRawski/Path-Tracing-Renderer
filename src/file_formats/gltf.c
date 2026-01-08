db27d06a358eaa237aac7fdc2baf57debef6998b
Maks Rawski <maksymilian.rawski@tutanota.com>
2026-01-08 14:49:38 +0100

    refactor: clearer load_gltf_scene

--- src/file_formats/gltf.c
+++ src/file_formats/gltf.c
@@ -17,56 +17,55 @@ void load_gltf_scene(Scene *scene, const char *path) {
   res = cgltf_load_buffers(&options, data, path);
   gltf_assert(res == cgltf_result_success, path, "%s\n", cgltf_result_str(res));
 
-  // === Preprocessing ===
-  unsigned int triangles_count = 0;
-  unsigned int mesh_primitives_count = 0;
+  // === Required capacity calculation ===
+  unsigned int max_meshes_count = data->meshes_count;
+  // there will always be a default material at index 0
+  unsigned int max_mats_count = data->materials_count + 1;
 
+  // NOTE: count_mesh_instances populates scene->mesh_instances_count
+  scene->mesh_instances_count = 0;
+  traverse_nodes(path, data, scene, count_mesh_instances);
+  unsigned int max_mesh_instances_count = scene->mesh_instances_count;
+
+  unsigned int max_triangles_count = 0;
+  unsigned int max_mesh_primitives_count = 0;
   for (cgltf_size m = 0; m < data->meshes_count; ++m) {
     cgltf_mesh mesh = data->meshes[m];
-    mesh_primitives_count += mesh.primitives_count;
+    max_mesh_primitives_count += mesh.primitives_count;
     for (cgltf_size p = 0; p < mesh.primitives_count; ++p) {
       cgltf_primitive prim = mesh.primitives[p];
       if (prim.type == cgltf_primitive_type_triangles)
-        triangles_count += prim.indices->count / 3;
+        max_triangles_count += prim.indices->count / 3;
     }
   }
 
-  // there will always be a default material at index 0
-  unsigned int mats_count = data->materials_count + 1;
-  unsigned int meshes_count = data->meshes_count;
+  // The worst case is when each leaf contains a single triangle/mesh (assuming
+  // empty nodes are impossible). In practice those functions shouldn't even
+  // create nodes this small. The formula is 2 * l - 1, where l is number of
+  // leaf nodes, but to avoid having the negative result in case when l is 0, 1
+  // is not subtracted.
+  unsigned int max_bvh_nodes_count = 2 * max_triangles_count;
+  unsigned int max_tlas_nodes_count = 2 * max_mesh_instances_count;
 
-  // NOTE: populates scene->mesh_instances_count
-  scene->mesh_instances_count = 0;
-  traverse_nodes(path, data, scene, count_mesh_instances);
-  unsigned int mesh_instances_count = scene->mesh_instances_count;
-
-  scene->camera = Camera_default();
-
-  // === Allocations ===
-  alloc_if_necessary((void **)&scene->triangles, triangles_count,
+  // === Memory allocations ===
+  alloc_if_necessary((void **)&scene->triangles, max_triangles_count,
                      sizeof(Triangle), &scene->triangles_capacity, false);
-  alloc_if_necessary((void **)&scene->mats, mats_count, sizeof(Material),
+  alloc_if_necessary((void **)&scene->mats, max_mats_count, sizeof(Material),
                      &scene->mats_capacity, true);
-  alloc_if_necessary((void **)&scene->mesh_primitives, mesh_primitives_count,
-                     sizeof(MeshPrimitive), &scene->mesh_primitives_capacity,
-                     true);
-  alloc_if_necessary((void **)&scene->meshes, meshes_count, sizeof(Mesh),
+  alloc_if_necessary((void **)&scene->mesh_primitives,
+                     max_mesh_primitives_count, sizeof(MeshPrimitive),
+                     &scene->mesh_primitives_capacity, true);
+  alloc_if_necessary((void **)&scene->meshes, max_meshes_count, sizeof(Mesh),
                      &scene->meshes_capacity, true);
-  alloc_if_necessary((void **)&scene->mesh_instances, mesh_instances_count,
+  alloc_if_necessary((void **)&scene->mesh_instances, max_mesh_instances_count,
                      sizeof(MeshInstance), &scene->mesh_instances_capacity,
                      true);
-
-  // Assuming that the worst case is each leaf containing a single triangle.
-  // In practice BVH_build function shouldn't even create nodes this small.
-  unsigned int bvh_nodes_count = 2 * triangles_count;
-  alloc_if_necessary((void **)&scene->bvh_nodes, bvh_nodes_count,
+  alloc_if_necessary((void **)&scene->bvh_nodes, max_bvh_nodes_count,
                      sizeof(BVHNode), &scene->bvh_nodes_capacity, true);
-
-  // similarly to the above case
-  unsigned int tlas_nodes_count = 2 * mesh_instances_count;
-  alloc_if_necessary((void **)&scene->tlas_nodes, tlas_nodes_count,
+  alloc_if_necessary((void **)&scene->tlas_nodes, max_tlas_nodes_count,
                      sizeof(TLASNode), &scene->tlas_nodes_capacity, true);
 
+  // === Scene initialization ===
   scene->triangles_count = 0;
   scene->bvh_nodes_count = 0;
   scene->mesh_primitives_count = 0;
