ff81b6c8284df1730e4528a0d090394e2e71850d
Maks Rawski <maksymilian.rawski@tutanota.com>
2026-01-09 02:02:12 +0100

    feat: add tone mapping

--- shaders/renderer.glsl
+++ shaders/renderer.glsl
@@ -448,6 +448,44 @@ vec3 GetColorForRay(Ray ray, inout uint rngState) {
     }
     return incomingLight;
 }
+// ACES tone mapping curve fit to go from HDR to LDR
+//https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+vec3 ACESFilm(vec3 x)
+{
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0f, 1.0f);
+}
+
+// sRGB
+vec3 LessThan(vec3 f, float value) {
+    return vec3((f.x < value) ? 1.0f : 0.0f,
+        (f.y < value) ? 1.0f : 0.0f,
+        (f.z < value) ? 1.0f : 0.0f);
+}
+
+vec3 LinearToSRGB(vec3 rgb) {
+    rgb = clamp(rgb, 0.0f, 1.0f);
+
+    return mix(
+        pow(rgb, vec3(1.0f / 2.4f)) * 1.055f - 0.055f,
+        rgb * 12.92f,
+        LessThan(rgb, 0.0031308f)
+    );
+}
+
+vec3 SRGBToLinear(vec3 rgb) {
+    rgb = clamp(rgb, 0.0f, 1.0f);
+
+    return mix(
+        pow(((rgb + 0.055f) / 1.055f), vec3(2.4f)),
+        rgb / 12.92f,
+        LessThan(rgb, 0.04045f)
+    );
+}
 
 void main() {
     vec2 resolution = vec2(params.width, params.height);
@@ -482,6 +520,8 @@ void main() {
         totalIncomingLight += GetColorForRay(ray, rngState);
     }
     totalIncomingLight /= float(params.samples_per_pixel);
+    totalIncomingLight = ACESFilm(totalIncomingLight);
+    totalIncomingLight = LinearToSRGB(totalIncomingLight);
 
     if (frame_number > 0) {
         vec3 lastFrameColor = texture(backBufferTexture, gl_FragCoord.xy / resolution.xy).rgb;
