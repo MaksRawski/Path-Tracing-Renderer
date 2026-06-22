4c309a42c2101e6f3043cbc10ca9c4f335fdc1d3
Maks Rawski <maksymilian.rawski@tutanota.com>
2026-06-22 17:41:39 +0200

    refactor(Settings): sooo many more abstractions
    wonder what will come out of this...

--- src/gui/settings.c
+++ src/gui/settings.c
@@ -13,19 +13,105 @@
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 
-static void tooltip(const char *desc) {
-  igSameLine(0.0f, -1.0f);
-  igTextDisabled("(?)");
-  if (igBeginItemTooltip()) {
-    igPushTextWrapPos(igGetFontSize() * 35.0f);
-    igTextUnformatted(desc, NULL);
-    igPopTextWrapPos();
-    igEndTooltip();
+// NOTE: current idea is to make it so that file position corresponds to level
+// of abstraction. The higher you are, the more abstract everything is.
+// NOTE: all static functions in this file perform drawing operations.
+// NOTE: everything should be aggressively inlined, there is no reason not to
+
+static inline void scene(AppState *state);
+static inline void camera(AppState *state);
+static inline void rendering(AppState *state);
+static inline void misc(GUIOverlay *gui, AppState *state);
+
+void GuiSettings_draw(GUIOverlay *gui, AppState *state) {
+  igBegin("Settings", NULL, 0);
+
+  if (igCollapsingHeader_TreeNodeFlags("Scene",
+                                       ImGuiTreeNodeFlags_DefaultOpen)) {
+    scene(state);
   }
+  if (igCollapsingHeader_TreeNodeFlags("Camera", 0)) {
+    camera(state);
+  }
+  if (igCollapsingHeader_TreeNodeFlags("Rendering", 0)) {
+    rendering(state);
+  }
+  if (igCollapsingHeader_TreeNodeFlags("Misc",
+                                       ImGuiTreeNodeFlags_DefaultOpen)) {
+    misc(gui, state);
+  }
+
+  igEnd();
 }
 
-static void GuiSettings_Scene(AppState *state) {
+static void tooltip(const char *desc);
+
+static inline void scene_load_button(AppState *state);
+static inline void scene_bvh_type_combobox(AppState *state);
+static inline void scene_stats(AppState *state);
+static inline void scene(AppState *state) {
+  scene_load_button(state);
+  scene_bvh_type_combobox(state);
+
+  scene_stats(state);
+}
+
+static inline void camera_position(AppState *state);
+static inline void camera_rotation(AppState *state);
+static inline void camera_fov(AppState *state);
+static inline void camera_speed(AppState *state);
+static inline void camera(AppState *state) {
+  igTextWrapped("To move the camera using keyboard (WASD keys) and mouse: "
+                "Hold the left mouse button over the rendered scene "
+                "(outside this Settings window).");
+  camera_position(state);
+  camera_rotation(state);
+  camera_fov(state);
+  camera_speed(state);
+}
+
+static inline bool rendering_env_color(AppState *state);
+static inline bool rendering_max_bounce_count(AppState *state);
+static inline bool rendering_samples_per_pixel(AppState *state);
+static inline bool rendering_diverge_strength(AppState *state);
+static inline bool rendering_frames_to_render(AppState *state);
+static inline bool rendering_resolution(AppState *state);
+static inline void rendering_stats(AppState *state);
+static inline void rendering(AppState *state) {
+  bool rendering_param_changed = false;
+
+  rendering_param_changed |= rendering_env_color(state);
+  rendering_param_changed |= rendering_max_bounce_count(state);
+  rendering_param_changed |= rendering_samples_per_pixel(state);
+  rendering_param_changed |= rendering_diverge_strength(state);
+  rendering_param_changed |= rendering_frames_to_render(state);
+  rendering_param_changed |= rendering_resolution(state);
+
+  if (rendering_param_changed) {
+    state->pending_actions |= Action_update_ssbo_renderer_parameters;
+  }
+  rendering_stats(state);
+}
+
+static inline void misc_ui_scale(GUIOverlay *gui);
+static inline void misc_scaling(AppState *state);
+static inline void misc_saved_image_path(AppState *state);
+static inline void misc(GUIOverlay *gui, AppState *state) {
+  misc_ui_scale(gui);
+  misc_scaling(state);
+  misc_saved_image_path(state);
+
+#ifndef NDEBUG
+  igCheckbox("Show ImGui demo", &gui->show_demo);
+  if (gui->show_demo)
+    igShowDemoWindow(NULL);
+#endif // NDEBUG
+}
+
+// === SCENE ===
+static inline void scene_load_button(AppState *state) {
   ImVec2 button_size = {.x = 0, .y = 0};
+  // TODO: enable this button only if on windows or zenity is installed
   if (igButton("Load scene", button_size)) {
     SmallString chosen_path = SmallString_new("");
     if (GuiFileBrowser_open(chosen_path.str, sizeof(SmallString))) {
@@ -33,12 +119,16 @@ static void GuiSettings_Scene(AppState *state) {
       state->pending_actions |= Action_load_scene;
     }
   }
+}
 
+static inline void scene_bvh_type_combobox(AppState *state) {
   if (igCombo_Str_arr("BVH type", (int *)&state->settings.BVH_build_strat,
                       BVHStrategy_str, BVHStrategy__COUNT, 5)) {
     state->pending_actions |= Action_build_bvh;
   }
+}
 
+static inline void scene_stats(AppState *state) {
   // if scene is loaded
   if (!SmallString_is_empty(&state->settings.scene_path)) {
     igText("Loaded scene: %s",
@@ -53,11 +143,8 @@ static void GuiSettings_Scene(AppState *state) {
   }
 }
 
-static void GUISettings_Camera(AppState *state) {
-  igTextWrapped("To move the camera using keyboard (WASD keys) and mouse: "
-                "Hold the left mouse button over the rendered scene "
-                "(outside this Settings window).");
-  // position
+// === CAMERA ===
+static inline void camera_position(AppState *state) {
   float cam_pos[3] = {state->settings.cam.pos.x, state->settings.cam.pos.y,
                       state->settings.cam.pos.z};
   if (igInputFloat3("Position", cam_pos, "%6.3f", 0)) {
@@ -66,8 +153,9 @@ static void GUISettings_Camera(AppState *state) {
     state->settings.cam.pos.y = cam_pos[1];
     state->settings.cam.pos.z = cam_pos[2];
   }
+}
 
-  // rotation
+static inline void camera_rotation(AppState *state) {
   YawPitch yp = YawPitch_from_dir(Vec3d_from_vec3(state->settings.cam.dir));
   bool dir_changed = false;
 
@@ -88,15 +176,18 @@ static void GUISettings_Camera(AppState *state) {
     state->settings.cam.dir = Vec3d_to_vec3(YawPitch_to_dir(yp));
     state->pending_actions |= Action_update_ssbo_camera;
   }
+}
 
-  // additional properties
+static inline void camera_fov(AppState *state) {
   float fov_deg = Camera_get_fov_deg(&state->settings.cam);
   if (igSliderFloat("FOV", &fov_deg, CAMERA_FOV_MIN_DEG, CAMERA_FOV_MAX_DEG,
                     "%3.1f", 0)) {
     Camera_set_fov_deg(&state->settings.cam, fov_deg);
     state->pending_actions |= Action_update_ssbo_camera;
   }
+}
 
+static inline void camera_speed(AppState *state) {
   igSliderFloat("Movement speed", &state->settings.cam.step_size_per_second,
                 CAMERA_MOVE_SPEED_PER_SECOND_MIN,
                 CAMERA_MOVE_SPEED_PER_SECOND_MAX, "%.2f", 0);
@@ -105,47 +196,63 @@ static void GUISettings_Camera(AppState *state) {
                 "%.4f", ImGuiSliderFlags_Logarithmic);
 }
 
-static void GuiSettings_Rendering(AppState *state) {
-  bool rendering_param_changed = false;
+// === RENDERING ===
+static inline bool rendering_env_color(AppState *state) {
+  return igColorEdit3("Environment color",
+                      state->settings.rendering_params.env_color, 0);
+}
 
-  rendering_param_changed |= igColorEdit3(
-      "Environment color", state->settings.rendering_params.env_color, 0);
-
-  rendering_param_changed |=
+static inline bool rendering_max_bounce_count(AppState *state) {
+  bool changed =
       igInputInt("Max Bounce Count",
                  &state->settings.rendering_params.max_bounce_count, 1, 1, 0);
   tooltip("Maximum number of bounces a ray can do before terminating.");
+  return changed;
+}
 
-  rendering_param_changed |=
+static inline bool rendering_samples_per_pixel(AppState *state) {
+  bool changed =
       igInputInt("Samples Per Pixel",
                  &state->settings.rendering_params.samples_per_pixel, 1, 1, 0);
   tooltip("How many rays should be sent for each pixel in a single rendering "
           "pass.");
+  return changed;
+}
 
-  rendering_param_changed |= igSliderFloat(
+static inline bool rendering_diverge_strength(AppState *state) {
+  bool changed = igSliderFloat(
       "Diverge strength", &state->settings.rendering_params.diverge_strength,
       0.0, 1, "%6.5f", ImGuiSliderFlags_Logarithmic);
   tooltip("How much should the rays sent from the same pixel differ in "
           "a single sample. The higher the value the more blurry the image "
           "becomes. Serves as a simple form of antialiasing.");
+  return changed;
+}
 
-  rendering_param_changed |=
+static inline bool rendering_frames_to_render(AppState *state) {
+  bool changed =
       igInputInt("Frames to render (-1 for inf)",
                  &state->settings.rendering_params.frames_to_render, 1, 1, 0);
   tooltip("A control for progressive rendering. This number of frames will be "
           "rendered and immediately averaged, effectively causing more "
           "rays to be sent from each pixel over the span of many frames.");
+  return changed;
+}
 
+static inline bool rendering_resolution(AppState *state) {
   int res[2] = {state->settings.rendering_params.rendering_resolution.width,
                 state->settings.rendering_params.rendering_resolution.height};
-
-  rendering_param_changed |= igInputInt2("Resolution", res, 0);
+  bool changed = igInputInt2( //
+      "Resolution", res, 0);
   tooltip("The resolution for which the image should be rendered. NOTE: this "
           "is independent of window size! Look at the Misc section for Scaling "
           "settings.");
   state->settings.rendering_params.rendering_resolution.width = res[0];
   state->settings.rendering_params.rendering_resolution.height = res[1];
+  return changed;
+}
 
+static inline void rendering_stats(AppState *state) {
   igText("Rendering last frame took: %s",
          Stats_display(state->stats.last_frame_rendering.total_time).str);
 
@@ -163,53 +270,36 @@ static void GuiSettings_Rendering(AppState *state) {
     break;
   }
   igText("Rendered frames: %d", state->stats.frame_number);
-
-  if (rendering_param_changed) {
-    state->pending_actions |= Action_update_ssbo_renderer_parameters;
-  }
 }
 
-static void GuiSettings_Misc(GUIOverlay *gui, AppState *state) {
+// === MISC ===
+static inline void misc_ui_scale(GUIOverlay *gui) {
   if (igSliderFloat("UI scale", &gui->ui_scale, 0.5, 2.5, "%3.2f", 0))
     GUIOverlay_scale(gui, gui->ui_scale);
+}
 
+static inline void misc_scaling(AppState *state) {
   igCombo_Str_arr("Scaling", (int *)&state->settings.scaling_mode,
                   WindowScalingMode_str, WindowScalingMode__COUNT, 5);
   tooltip("Method to use for displaying the rendered image when the window "
           "size is different from the Rendering Resolution");
-
+}
+static inline void misc_saved_image_path(AppState *state) {
   igInputText("Saved image path", state->settings.saved_image_path.str,
               sizeof(state->settings.saved_image_path), 0, NULL, NULL);
   ImVec2 button_size = {.x = 0, .y = 0};
   if (igButton("Save image", button_size)) {
     state->pending_actions |= Action_save_image;
   }
-
-#ifndef NDEBUG
-  igCheckbox("Show ImGui demo", &gui->show_demo);
-#endif
-
-  if (gui->show_demo)
-    igShowDemoWindow(NULL);
 }
 
-void GuiSettings_draw(GUIOverlay *gui, AppState *state) {
-  igBegin("Settings", NULL, 0);
-
-  if (igCollapsingHeader_TreeNodeFlags("Scene",
-                                       ImGuiTreeNodeFlags_DefaultOpen)) {
-    GuiSettings_Scene(state);
+static void tooltip(const char *desc) {
+  igSameLine(0.0f, -1.0f);
+  igTextDisabled("(?)");
+  if (igBeginItemTooltip()) {
+    igPushTextWrapPos(igGetFontSize() * 35.0f);
+    igTextUnformatted(desc, NULL);
+    igPopTextWrapPos();
+    igEndTooltip();
   }
-  if (igCollapsingHeader_TreeNodeFlags("Camera", 0)) {
-    GUISettings_Camera(state);
-  }
-  if (igCollapsingHeader_TreeNodeFlags("Rendering", 0)) {
-    GuiSettings_Rendering(state);
-  }
-  if (igCollapsingHeader_TreeNodeFlags("Misc",
-                                       ImGuiTreeNodeFlags_DefaultOpen)) {
-    GuiSettings_Misc(gui, state);
-  }
-
-  igEnd();
 }
