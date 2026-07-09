19b3dffbf1c72ff30acc655df070cb2fc13af936
Maks Rawski <maksymilian.rawski@tutanota.com>
2026-07-10 00:14:37 +0200

    cleanup(small_string): remove unnecessary assert
    it even caused issues on empty strings

--- src/small_string.c
+++ src/small_string.c
@@ -1,11 +1,10 @@
 #include "small_string.h"
-#include "asserts.h"
 #include <string.h>
 
 SmallString SmallString_new(const char *str) {
-  ASSERTQ_CUSTOM(str != NULL, "SmallString_new_from called with NULL!");
   SmallString res = {0};
-  strncpy(res.str, str, sizeof(res.str));
+  if (str[0] != 0)
+    strncpy(res.str, str, sizeof(res.str));
   return res;
 }
 
