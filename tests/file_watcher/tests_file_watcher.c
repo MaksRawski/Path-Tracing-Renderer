5aec885847b887d2db0ab81f2bc648ba81626d2f
Maks Rawski <maksymilian.rawski@tutanota.com>
2026-07-10 20:20:19 +0200

    fix(tests): remove unnecessary include
    unistd.h is a POSIX library so it doesn't compile on windows

--- tests/file_watcher/tests_file_watcher.c
+++ tests/file_watcher/tests_file_watcher.c
@@ -3,13 +3,12 @@
 #include "tests_macros.h"
 #include "utils/file_watcher.h"
 #include <stdio.h>
-#include <unistd.h>
 
 bool test_FileWatcher__basic(void) {
   const char *path = "test_FileWatcher__basic.txt";
   FILE *f = fopen(path, "w");
-  FileWatcher fw = FileWatcher_new(path);
   ASSERT_CUSTOM(f != NULL, "Failed to open file!");
+  FileWatcher fw = FileWatcher_new(path);
 
   ASSERT_CUSTOM(!FileWatcher_did_change(&fw), "No changes were yet expected.");
   ASSERT_CUSTOM(!FileWatcher_did_change(&fw), "No changes were yet expected.");
